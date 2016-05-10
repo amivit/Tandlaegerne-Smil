1dc44ac0acdf5ce7bc9016a60f32245a93a409e7
Amivit <git@wittig.dk>
2016-05-10 13:43:21 +0200

    Første commit med DB klar

--- Tandlægerne Smil/Program.cs
+++ Tandlægerne Smil/Program.cs
@@ -6,13 +6,13 @@ using System.Windows.Forms;
 
 namespace Tandlægerne_Smil
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
