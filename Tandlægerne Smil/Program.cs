2f8bd5cb893b6fb350dea6618e7db2fb40315693
Amivit <git@wittig.dk>
2016-05-10 14:11:09 +0200

    Paul test commit

--- Tandlægerne Smil/Program.cs
+++ Tandlægerne Smil/Program.cs
@@ -14,6 +14,7 @@ namespace Tandlægerne_Smil
         [STAThread]
         private static void Main()
         {
+            // test
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
