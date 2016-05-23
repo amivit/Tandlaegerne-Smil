66605021d3d20a534be717ded3e1970a27fabd5d
Kiil <Nikolajkiil@live.dk>
2016-05-23 11:59:39 +0200

    Opret/redigere booking af patient //WIP

--- Tandlægerne Smil/Models/Book.cs
+++ Tandlægerne Smil/Models/Book.cs
@@ -5,17 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Tandlægerne_Smil.Controllers.DbController;
+using Tandlægerne_Smil.Views;
 
 namespace Tandlægerne_Smil.Models
 {
     internal class Book : Global
     {
         private readonly BookingDb _bookingDb = new BookingDb();
-
-        public void OpretBookning()
-        {
-            throw new System.NotImplementedException();
-        }
 
         public void AkutAnkomst()
         {
@@ -34,5 +30,41 @@ namespace Tandlægerne_Smil.Models
             var bookings = Db.BookingDbs.ToList();
             Console.WriteLine(bookings[index].Tidspunkt.ToShortDateString());
         }
-    }
+
+		public void LoadOpretBooking(int patientID, BookingOpretRedigere bookingOpretRedigere)
+		{
+			
+			var patient = Db.PatientDbs.FirstOrDefault(p => p.PatientId == patientID);
+			bookingOpretRedigere.textBoxPatient.Text = patient.Fornavn + " " + patient.Efternavn;
+			bookingOpretRedigere.textBoxNoter.Text = patient.Noter;
+
+			
+			var læger = Db.AnsatDbs.ToList();
+			var lokaler = Db.BehandlingsrumDbs.ToList();
+			var behandlinger = Db.BehandlingDbs.ToList();
+
+			var indexNavn = 0;
+			var indexRum = 0;
+			var indexbehandlinger = 0;
+			foreach (var item in Db.AnsatDbs)
+			{
+
+				bookingOpretRedigere.comboBoxLæge.Items.Add(læger[indexNavn].Fornavn + " " + læger[indexNavn].Efternavn);
+				indexNavn ++;
+			}
+			foreach (var item in Db.BehandlingsrumDbs)
+			{
+				bookingOpretRedigere.comboBoxLokale.Items.Add(lokaler[indexRum].RumNavn);
+				indexRum ++;
+			}
+
+			foreach (var item in Db.BehandlingDbs)
+			{
+				bookingOpretRedigere.comboBoxBehandling.Items.Add(behandlinger[indexbehandlinger].Navn);
+				indexbehandlinger ++;
+			}
+
+		}
+
+	}
 }
--- Tandlægerne Smil/Views/BookingOpretRedigere.cs
+++ Tandlægerne Smil/Views/BookingOpretRedigere.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tandlægerne_Smil.Controllers;
 
 namespace Tandlægerne_Smil.Views
 {
@@ -14,17 +15,25 @@ namespace Tandlægerne_Smil.Views
     {
         private StartForm _startForm;
         readonly int _patientId;
+		private readonly Controller _controller = new Controller(); // Så vores view kan snakke med controllerenPatient P = new Patient();
 
-        public BookingOpretRedigere(int patientId, StartForm startForm)
+
+		public BookingOpretRedigere(int patientId, StartForm startForm)
         {
             this._patientId = patientId;
             this._startForm = startForm;
             InitializeComponent();
-        }
+			_controller.Book.LoadOpretBooking(patientId, this);
+
+	    
+			
+		       
+               
+		}
 
         private void BookingOpretRedigere_Load(object sender, EventArgs e)
         {
-
+		
         }
     }
 }
